75e624377f437508a57b360ddd7fc601eda9b6a6
BrianTukei <briantukei1000@gmail.com>
2024-04-28 22:42:42 -0700

    Python script that fetches

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-
-Usage: ./1-hbtn_header.py <URL>
 """
-import sys
-import urllib.request
+Fetches https://intranet.hbtn.io/status
+use the package urllib
+ body of the response must be displayed in tabulation before -
+ """
+ import urllib.request
+ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+         html = response.read()
 
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+         print('Body response:\n\t- type: {}'.format(type(html)))
+         print('\t- content: {}'.format(html))
+         print('\t- utf8 content: {}'.format(html.decode('utf-8')))
