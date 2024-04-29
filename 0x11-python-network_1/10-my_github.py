caf6d5e9d3f4125f67669d66b02494efe86fa275
BrianTukei <briantukei1000@gmail.com>
2024-04-29 01:18:33 -0700

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""Sends a search request for a given string to the Star Wars API.
+"""Python script that takes GitHub credentials (username and
+password) and uses the GitHub API to display the user id."""
 
-Usage: ./9-starwars.py <search string>
-  - The search request is sent to the Star Wars API search people endpoint.
-  """
-  import sys
-  import requests
+import requests
+from sys import argv
 
-
-  if __name__ == "__main__":
-      url = "https://swapi.co/api/people"
-      params = {"search": sys.argv[1]}
-      results = requests.get(url, params=params).json()
-
-      print("Number of results: {}".format(results.get("count")))
-      [print(r.get("name")) for r in results.get("results")]
+if __name__ == "__main__":
+    auth = (argv[1], argv[2])
+    request = requests.get("https://api.github.com/user", auth=auth)
+    print(request.json().get("id"))
