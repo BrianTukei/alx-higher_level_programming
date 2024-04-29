476e08743976488c8584ddd867fba0c276218888
BrianTukei <briantukei1000@gmail.com>
2024-04-29 00:52:20 -0700

    Search

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
-"""
-if __name__ == '__main__':
-    import requests
-    from sys import argv
-    if len(argv) == 2:
-        q = argv[1]
-    else:
-        q = ""
-        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-        try:
-            r_dict = r.json()
+"""Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
 
-            id = r_dict.get('id')
+import requests
+from sys import argv
 
-            name = r_dict.get('name')
+if __name__ == "__main__":
+    data = {"q": argv[1] if len(argv) > 1 else ""}
+    request = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        json = request.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
 
-            if len(r_dict) == 0 or not id or not name:
-
-                print("No result")
-            else:
-                print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-        except:
-            print("Not a valid JSON")
+            print("No result")
+    except:
+        print("Not a valid JSON")
