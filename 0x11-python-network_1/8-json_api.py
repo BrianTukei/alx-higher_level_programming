b567116a26689669b5b35d65bff0388daadbaa2a
BrianTukei <briantukei1000@gmail.com>
2024-05-08 09:19:49 -0700

    Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-''' Search API '''
-import requests
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
 import sys
+import requests
 
 
-if __name__ == '__main__':
-    data = {'q': ''}
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        data['q'] = sys.argv[1]
-    except:
-        pass
-
-    res = requests.post('http://0.0.0.0:5000/search_user', data)
-
-    try:
-        jsonify = res.json()
-        if not jsonify:
-            print('No result')
+        response = r.json()
+        if response == {}:
+            print("No result")
         else:
-            print('[{}] {}'.format(jsonify.get('id'), jsonify.get('name')))
-    except:
-        print('Not a valid JSON')
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
